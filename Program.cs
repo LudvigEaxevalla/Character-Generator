69ea9b044841f330800ef2f35f570b057883cdd8
LudvigEaxevalla <aspel25luei@axevalla.fhsk.se>
2025-12-21 23:37:35 +0100

    more stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.NetworkInformation;
+using System.IO;
+
 
 Random rnd = new Random();
 string name = "";
@@ -7,16 +9,30 @@ int ageDice = rnd.Next(15,90);
 int dice = rnd.Next(1,20);
 
 
-string[] gender = {"Female", "Male", "Non-Binary", "Non specified"};
+string[] gender = {"Female", "Male", "Non-Binary", "Non specified", "Will add later"};
 int selectedGender;
+string[] personalityTrait = {"Introverted", "Extroverted", "Ambiverted", "Wildcard", "Will add later"};
+int selectedPersonalityTrait;
+string[] desires = {"Power", "Wealth", "Calmness", "Chaos", "Peace", "None", "Will add later"};
+int selectedDesire;
+string[] beliefe = {"Higher Power", "Humanity", "Control", "Chaos Theory", "Kindness", "Nothing", "Will add later"};
+int selectedBelife;
+string[] dislikes = {"Religion", "Greed", "Bonfires", "Chaos", "Peace", "Evil", "Happiness", "Will add later"};
+//string savedCharachters = File.ReadAllText("/home/FuxiT/Documents/Character-Generator/Charachters/");
+int selectedDislikes;
 int stammina;
 int charisma;
 int strength;
 int durability;
+int luck;
+int recklessness;
+int stubborness;
+string country = "";
+
 int education;
 int intelligence; 
-int weaponSkill;
 int age;
+string description = "";
 
 int exitOption;
 
@@ -25,13 +41,30 @@ bool teen = false;
 bool youngAdult = false;
 bool adult = false;
 bool old = false;
+bool validName = false;
 
 
 Console.Clear();
+
 Console.WriteLine("Choose a name for your charachter");
 
-name = Console.ReadLine()!;
-path = "/home/leinarsson/Documents/Programmering/Charachter Generator/Charachters/" + name;
+
+while (!validName)
+{
+    
+    name = Console.ReadLine()!;
+    if (name == "")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You must type in a name");
+        Console.ResetColor();
+    }
+    else
+    {
+        validName = true;
+    }
+}
+path = "/home/FuxiT/Documents/Character-Generator/Charachters/" + name;
 
 if (File.Exists(path))
 {
@@ -39,6 +72,7 @@ if (File.Exists(path))
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("Heads up!");
     Console.WriteLine("A charachter with this name already exists!");
+   // Console.WriteLine(savedCharachters, name);
     Console.WriteLine("Saving this will override the existing file with the same name");
     Console.ResetColor();
     Console.WriteLine("1.Continue / 2.Exit program"); 
@@ -63,6 +97,14 @@ if (File.Exists(path))
 
 }
 
+
+Console.Clear();
+Console.WriteLine("Where is your character from? (optional)");
+country = Console.ReadLine()!;
+
+
+
+//Gender
 Console.Clear();
 Console.WriteLine("Choose a gender");
 for (int i = 0; i < gender.Length; i++)
@@ -71,11 +113,56 @@ for (int i = 0; i < gender.Length; i++)
 }
 selectedGender = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(gender[selectedGender-1]);
+Console.ReadKey();
+
+
+
+//Trait
+Console.Clear();
+Console.WriteLine("Choose personality trait");
+for (int i = 0; i < personalityTrait.Length; i++)
+{
+    Console.WriteLine(i+1 + " " + personalityTrait[i]);
+}
+selectedPersonalityTrait = int.Parse(Console.ReadLine()!);
+
+
+
+//Desire
+Console.Clear();
+Console.WriteLine("What does your character desire?");
+for (int i = 0; i < desires.Length; i++)
+{
+    Console.WriteLine(i+1 + " " + desires[i]);
+}
+selectedDesire = int.Parse(Console.ReadLine()!);
+
+
+
+//Belives in
+Console.Clear();
+Console.WriteLine("What does you character belive in?");
+for (int i = 0; i < beliefe.Length; i++)
+{
+    Console.WriteLine(i+1 + " " + beliefe[i]);
+}
+selectedBelife = int.Parse(Console.ReadLine()!);
+
+
+
+//Dislikes
+Console.Clear();
+Console.WriteLine("What does your character dislike?");
+for (int i = 0; i < dislikes.Length; i++)
+{
+    Console.WriteLine(i+1 + " " + dislikes[i]);
+}
+selectedDislikes = int.Parse(Console.ReadLine()!);
+
+
 
 void RollDice()
 {
-    Console.Clear();
     dice = rnd.Next(1,20);
     for (int i = 0; i < dice; i++)
     {
@@ -90,60 +177,86 @@ void RollDice()
 
 void Stammina()
 {
-    Console.Clear();
-    Console.WriteLine("Roll dice for stammina!");
-    Console.ReadKey();
+    Console.WriteLine("Rolling stammina...");
     RollDice();
-    Console.WriteLine("\n"+ dice);
-    stammina = dice*7;
-    Console.ReadKey();
+    stammina = dice*5;
+    Console.WriteLine("\nStammina: "+ stammina);
+    Console.WriteLine("");
+
 }
 
 void Strength()
 {
-    Console.Clear();
-    Console.WriteLine("Roll dice for strength!");
-    Console.ReadKey();
+    Console.WriteLine("Rolling strength...");
     RollDice();
-    Console.WriteLine("\n"+ dice);
     strength = dice*4;
-    Console.ReadKey();
+    Console.WriteLine("\nStrength: "+ strength);
+    Console.WriteLine("");
+
 }
 
 void Charisma()
 {
-    Console.Clear();
-    Console.WriteLine("Roll dice for charisma!");
-    Console.ReadKey();
+    Console.WriteLine("Rolling charisma...");
     RollDice();
-    Console.WriteLine("\n"+ dice);
     charisma = dice*5;
-    Console.ReadKey();
+    Console.WriteLine("\nCharisma: "+ charisma);
+    Console.WriteLine("");
+
 }
 
 
 void Education()
 {
-    Console.Clear();
-    Console.WriteLine("Roll dice for education!");
-    Console.ReadKey();
+    Console.WriteLine("Rolling education...");
     RollDice();
-    Console.WriteLine("\n"+ dice);
-    education = dice*7;
-    Console.WriteLine("Education: " + education);
-    Console.ReadKey();
+    education = dice*4;
+    Console.WriteLine("\nEducation: " + education);
+    Console.WriteLine("");
+
 }
 
 void Intelligence()
 {
-    Console.Clear();
-    Console.WriteLine("Roll dice for intelligence!");
-    Console.ReadKey();
+    Console.WriteLine("Rolling intelligence...");
     RollDice();
-    Console.WriteLine("\n"+ dice);
-    intelligence = education/dice*11;
-    Console.ReadKey();
+    intelligence = education+dice;
+    Console.WriteLine("\nIntelligence: " + intelligence);}
+    Console.WriteLine("");
+
+
+void Luck()
+{
+
+    Console.WriteLine("Rolling luck...");
+    RollDice();
+    luck = dice*5;
+    Console.WriteLine("\nLuck: " + luck);
+    Console.WriteLine("");
+
 }
+
+void Recklessness()
+{
+
+    Console.WriteLine("Rolling recklessness...");
+    RollDice();
+    recklessness = dice*5;
+    Console.WriteLine("\nRecklessness: " + recklessness);
+    Console.WriteLine("");
+
+}
+
+void Stubborness()
+{
+
+    Console.WriteLine("Rolling stubborness...");
+    RollDice();
+    stubborness = dice*5;
+    Console.WriteLine("\nStubborness: " + stubborness);
+    Console.WriteLine("");
+}
+
 
 void RollAge()
 {
@@ -188,25 +301,42 @@ void Age()
     }
 }
 
+Console.Clear();
 Stammina();
 Strength();
 durability = stammina + strength / 4;
+Console.WriteLine("Durability: " + durability);
+Console.WriteLine("");
 Charisma();
 Education();
 Intelligence();
+Console.WriteLine("");
+Luck();
+Recklessness();
+Stubborness();
+Console.ReadKey();
 
 Console.Clear();
 
 Console.WriteLine("Name: " + name);
+Console.WriteLine("From: " + country);
 Console.WriteLine("Gender: " + gender[selectedGender-1]);
-Console.WriteLine("Stammina " + stammina);
+Console.WriteLine("Personality Trait: " + personalityTrait[selectedPersonalityTrait-1]);
+Console.WriteLine("Desire: " + desires[selectedDesire-1]);
+Console.WriteLine("Belives in: " + beliefe[selectedBelife-1]);
+Console.WriteLine("Dislikes: " + dislikes[selectedDislikes-1]);
+Console.WriteLine("Stammina: " + stammina);
 Console.WriteLine("Strength: " + strength);
 Console.WriteLine("Durability: " + durability);
-Console.WriteLine("Charisma " + charisma);
+Console.WriteLine("Charisma: " + charisma);
 Console.WriteLine("Education: " + education);
 Console.WriteLine("Intelligence: " + intelligence);
-
-Console.WriteLine("Now lets change the values by rolling your age");
+Console.WriteLine("Luck: " + luck);
+Console.WriteLine("Recklessness: " + recklessness);
+Console.WriteLine("Stubborness: " + stubborness);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nNow lets change the values by rolling your age");
+Console.ResetColor();
 Age();
 
 if (teen)
@@ -217,6 +347,8 @@ if (teen)
     charisma -= 30;
     education -= 30;
     intelligence /= 2;
+    recklessness += 20;
+    stubborness += 20;
     Console.WriteLine("Teen");
 }
 
@@ -228,6 +360,7 @@ else if (youngAdult)
     charisma += 10;
     education += 30;
     intelligence -= 10;
+    stubborness -= 20;
     Console.WriteLine("Young Adult");
 
 }
@@ -240,6 +373,7 @@ else if (adult)
     charisma += 30;
     education += 30;
     intelligence += 20;
+    stubborness += 30;
     Console.WriteLine("Adult");
 
 }
@@ -251,20 +385,35 @@ else if (old)
     durability = stammina - strength + 10;
     education -= 10;
     intelligence *= 2;
+    stubborness *= 2;
     Console.WriteLine("Old");
 
 }
 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Write a short description about your character (optional)");
+description = Console.ReadLine()!;
+Console.ResetColor();
+
 
 Console.WriteLine("Name: " + name);
+Console.WriteLine("From: " + country);
 Console.WriteLine("Gender: " + gender[selectedGender-1]);
-Console.WriteLine("Age; " + age);
-Console.WriteLine("Stammina " + stammina);
+Console.WriteLine("Personality Trait: " + personalityTrait[selectedPersonalityTrait-1]);
+Console.WriteLine("Desire: " + desires[selectedDesire-1]);
+Console.WriteLine("Belives in: " + beliefe[selectedBelife-1]);
+Console.WriteLine("Dislikes: " + dislikes[selectedDislikes-1]);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Stammina: " + stammina);
 Console.WriteLine("Strength: " + strength);
 Console.WriteLine("Durability: " + durability);
 Console.WriteLine("Charisma " + charisma);
 Console.WriteLine("Education: " + education);
 Console.WriteLine("Intelligence: " + intelligence);
+Console.WriteLine("Luck: " + luck);
+Console.WriteLine("Recklessness: " + recklessness);
+Console.WriteLine("Stubborness: " + stubborness);
+Console.WriteLine("Description: " + description);
 
 
 
@@ -279,9 +428,32 @@ while (!saveOption)
     
     if (save == "y" || save == "Y")
     {
-        File.WriteAllText(path, "Name: " + name + "\nGender: " + gender[selectedGender-1] + "\nAge: " + age + "\nStammina: " + stammina + "\nStrength: " + strength + "\nDurability: " + durability + "\nCharisma: " + charisma + "\nEducation: " + education + "\nIntelligance: " + intelligence);
+        File.WriteAllText(path, "Name: " + name +
+        "\nFrom: " + country +
+        "\nGender: " + gender[selectedGender-1] +
+        "\nPersonality Trait: " + personalityTrait[selectedPersonalityTrait-1] +
+        "\nDesire: " + desires[selectedDesire] +
+        "\nBelives in: " + beliefe[selectedBelife] +
+        "\nDislike: " + dislikes[selectedDislikes] +
+        "\nAge: " + age +
+        "\nStammina: " + stammina +
+        "\nStrength: " + strength +
+        "\nDurability: " + durability + 
+        "\nCharisma: " + charisma +
+        "\nEducation: " + education +
+        "\nIntelligance: " + intelligence +
+        "\nLuck: " + luck +
+        "\nRecklessness: " + recklessness +
+        "\nStubborness: " + stubborness +
+        "\nDescripton: '" + description+ "'"  +
+        "\nDesire: " + desires[selectedDesire-1] +
+        "\nBelives in: " + beliefe[selectedBelife-1] +
+        "\nDislikes: " + dislikes[selectedDislikes-1]
+        );
+
+        Console.WriteLine("Saved as " + name + ".txt in " + path);
         saveOption = true;
-        Console.ReadKey();
+        Environment.Exit(0);
     }
 
     else if (save == "n" || save == "N")
--- obj/Debug/net9.0/Charachter Generator.AssemblyInfo.cs
+++ obj/Debug/net9.0/Charachter Generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Charachter Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11a133f9e420bffe966480e161610164767a7a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365ae02cf18db0dff79cf6a95a60b30c0ed48c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Charachter Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Charachter Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
